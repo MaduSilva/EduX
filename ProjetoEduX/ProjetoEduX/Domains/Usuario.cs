03f851617b8d0429e74833d3424ee9727f38099f
Mateus Mello <mt.gatti@hotmail.com>
2020-11-05 13:40:18 -0300

    Pontuacao adicionada

--- ProjetoEduX/ProjetoEduX/Domains/Usuario.cs
+++ ProjetoEduX/ProjetoEduX/Domains/Usuario.cs
@@ -16,6 +16,7 @@ namespace ProjetoEduX.Domains
         public string Nome { get; set; }
         public string Email { get; set; }
         public string Senha { get; set; }
+        public string Pontuacao { get; set; }
         public DateTime? DataCadastro { get; set; }
         public DateTime? DataUltimoAcesso { get; set; }
         public Guid? IdPerfil { get; set; }
